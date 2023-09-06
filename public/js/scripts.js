ecd4ceab552bcfa4c2643490d911e5206d3e78d9
Guillermo Vazquez <vazqueztguillermo@gmail.com>
2023-09-06 03:46:15 -0500

    First commit to master

---
+++ public/js/scripts.js
@@ -0,0 +1,20 @@
+
+
+function rollin() {
+  var reveals = document.querySelectorAll(".rollin");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", rollin);
+
